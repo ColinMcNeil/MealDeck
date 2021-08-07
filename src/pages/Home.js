640b0f8b260db8cea45bad0b7e546cbf32c5c9de
colinmcneil <colinmcneil@gmail.com>
2021-08-07 18:23:34 -0400

    Add basic homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import "./Home.css";
 
-export default () => <div>Home</div>;
+export default () => {
+  const [code, setCode] = useState(null);
+  return (
+    <div className="home">
+      <h1>MealDeck</h1>
+      <p>A helpful tool to plan meals</p>
+      <p>
+        Idea by Sarah Schmieder, developed by{" "}
+        <a href="https://colinmcneil.me/" target="_blank">
+          me
+        </a>
+      </p>
+      <a className="enter" href="/new">
+        Make a Meal
+      </a>
+      <p>OR</p>
+      <div className="paste">
+        <textarea
+          onChange={(e) => setCode(e.target.value)}
+          placeholder="Paste a code"
+        ></textarea>
+        {code && <button>Show me your deck</button>}
+      </div>
+    </div>
+  );
+};
